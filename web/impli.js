c883cb50de1a5b2078d02a4248110c3c87cd9dd2
bfeitknecht <bfeitknecht@ethz.ch>
2025-09-01 17:16:28 +0200

    some progress is being made

--- web/impli.js
+++ web/impli.js
@@ -1,1 +1,15 @@
 // TODO
+
+const wasm_url = "./impli.wasm";
+const glue_url = "./glue.js";
+const __exports = {};
+
+const { instance } = await WebAssembly.instantiateStreaming(fetch(wasm_url), {
+  ghc_wasm_jsffi: (await import(glue_url)).default(__exports),
+  // CHECK: what needs to be done here?
+  // wasi_snapshot_preview1: ...
+});
+
+Object.assign(__exports, wasm_instance.exports);
+
+// CHECK: how to export `instance`?
